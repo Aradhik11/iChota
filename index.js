25361506d9eff134eee136073a9b8a01bb084646
Dhikrullah Abdur-Rahmon <aradhik11@gmail.com>
2025-02-07 15:40:01 +0100

    new changes

--- index.js
+++ index.js
@@ -21,8 +21,12 @@ if (!fs.existsSync(uploadsDir)) {
 // Middleware
 app.use(bodyParser.json());
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: [
+        'https://ichota.onrender.com',
+        'http://localhost:5173'
+        ],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization']
   }));
 
